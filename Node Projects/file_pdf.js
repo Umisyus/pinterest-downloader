d1c3ac67d8ebbe8e9ad2880e20e4d553d453e948
umit <umit007_@hotmail.com>
2022-09-23 12:06:48 -0400

    Playwright

--- Node Projects/file_pdf.js
+++ Node Projects/file_pdf.js
@@ -2,7 +2,7 @@
 
 const path = require('path');
 const { pathToFileURL } = require('url');
-
+const pw = require('playwright')
 const fs = require('fs').promises;
 // open folder and read files
 let fileNames = []
@@ -23,22 +23,39 @@ async function file_list(SRC_DIR) {
         Promise.all(Array.from(
             dir.map(async directoryName => {
                 let fileNames = (await fs.readdir(`${SRC_DIR}/${directoryName}`))
-                // add source dir to path
-                    .map(file =>{
+                    // add source dir to path
+                    .map(file => {
                         let file_path = path.normalize(`${SRC_DIR}/${directoryName}/${file}`)
                         return pathToFileURL(file_path).toString()
                     })
                 return { directoryName, files: fileNames }
-         }))))
-        .catch(e => console.log(e)))
+            })))));
+    // .catch(e => console.log(e)))
 }
 
 
 (async () => {
-    let result = await file_list(SRC_DIR).then(r => console.log(r))
+    let result = (await file_list(SRC_DIR).then(r => r))
+        .filter(f => f !== null)
+    // Filter nulls ???
 
-    url_builder = (filename) =>{
+    // Read and PDF all files with Playwright
+    // console.log(result);
 
+    let context = (await (await pw.chromium.launch({ headless: true })).newContext())
+    let p = await context.newPage()
+
+    for await (const { directoryName, files } of result) {
+        for await (f of files) {
+            Promise.all([await p.goto(f, { waitUntil: 'load', timeout: 60000 }),
+            await p.pdf({ path: `./PDFs/${directoryName}/${f}` })])
+
+                .then(x => console.log(`Done.
+                \n ${(async () => (await x[0].json().then(x => x)))}
+                size is: ${x[1].byteLength}
+                `))
+        }
     }
+
 
 })()
