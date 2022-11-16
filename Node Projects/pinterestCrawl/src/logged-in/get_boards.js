0fe33392dcfec909ce24661f71a315d375501880
umit <umit007_@hotmail.com>
2022-11-16 01:09:41 -0500

    Organize more files

--- Node Projects/pinterestCrawl/src/logged-in/get_boards.js
+++
@@ -1,9 +0,0 @@
-/* Use when logged in */
-function get_boards(boards_selector = "div[data-test-id='pwt-grid-item']") {
-    // Check if null or empty
-    if (boards_selector == null || boards_selector == "")
-        boards_selector = "div[data-test-id='pwt-grid-item']"
-    let boards = $$(boards_selector)
-    let links = boards.map(i => i.querySelector('a')).map(i => i.href)
-    return links
-}
