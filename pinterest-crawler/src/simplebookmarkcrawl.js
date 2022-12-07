5172ffa09be7afc9a311e27b4825158f91d75720
umit <umit007_@hotmail.com>
2022-12-07 14:24:27 -0500

    Continue to end

--- pinterest-crawler/src/simplebookmarkcrawl.js
+++ pinterest-crawler/src/simplebookmarkcrawl.js
@@ -1,8 +1,6 @@
 // NODE VERSION
-let userName = 'USERNAME'
+let userName = 'USRENAME'
 let pins_url_bookmark = (userName, bookmark) => `https://www.pinterest.ca/resource/UserPinsResource/get/?source_url=%2F${userName}%2Fpins%2F&data=%7B%22options%22%3A%7B%22is_own_profile_pins%22%3Atrue%2C%22username%22%3A%22${userName}%22%2C%22field_set_key%22%3A%22grid_item%22%2C%22pin_filter%22%3Anull%2C%22bookmarks%22%3A%5B%22${bookmark}%22%5D%7D%2C%22context%22%3A%7B%7D%7D&_=1670393784068`
-
-pins_url_bookmark = (userName, bookmark) => `https://www.pinterest.ca/resource/UserPinsResource/get/?source_url=%2F${userName}%2Fpins%2F&data=%7B%22options%22%3A%7B%22is_own_profile_pins%22%3Atrue%2C%22username%22%3A%22${userName}%22%2C%22field_set_key%22%3A%22grid_item%22%2C%22pin_filter%22%3Anull%2C%22bookmarks%22%3A%5B%22${bookmark}%22%5D%7D%2C%22context%22%3A%7B%7D%7D&_=1670393784068`
 
 query = pins_url_bookmark(userName, '')
 async function writeToFile(data) { await fs.writeFile('results.json', data, () => { }) }
@@ -15,13 +13,11 @@ let timer = setInterval(async () => {
     query = pins_url_bookmark(userName, bookmark)
 
     let pins = response_json.resource_response.data
-    // console.info(`pins: ${pins.length}`)
-    // // console.log({ pins, bookmark })
     list.push(pins)
-    // console.log(pins);
 
-    // if (bookmark.includes('end')) {
-    if (i > 2) {
+    console.log(`list length: ${list.length}`);
+
+    if (bookmark.includes('end')) {
         console.log('end');
         writeToFile(JSON.stringify(list.flat()))
         console.log("saved to file");
