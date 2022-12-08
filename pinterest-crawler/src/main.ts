77ddb46f0caaa5b1262b02ccf918843dbe94b469
umit <umit007_@hotmail.com>
2022-12-07 20:43:06 -0500

    crawling

--- pinterest-crawler/src/main.ts
+++ pinterest-crawler/src/main.ts
@@ -6,7 +6,7 @@ import { Actor, Dataset } from 'apify';
 import { Datum } from './pin-board-data-type.js';
 export const CRAWLEE_CONSTANTS = { datastorename: 'pinterest-json', reqQueue: "pinterest", login: 'login', board: "board", section: "section", pin: "pin", download_pin: "dlpin" };
 
-const startUrls = ['https://pinterest.ca/dracana96/pins'];
+const startUrls = ['https://www.pinterest.ca/resource/UserPinsResource/get/?source_url=%2Fdracana96%2Fpins%2F&data=%7B%22options%22%3A%7B%22is_own_profile_pins%22%3Atrue%2C%22username%22%3A%22dracana96%22%2C%22field_set_key%22%3A%22grid_item%22%2C%22pin_filter%22%3Anull%7D%2C%22context%22%3A%7B%7D%7D&_=1670391885387'];
 const exportFileName = `unique-pins-export`;
 
 // let kvs = await KeyValueStore.open(`${CRAWLEE_CONSTANTS.datastorename}`);
@@ -44,28 +44,60 @@ const preNavigationHooks = [
 ];
 
 await Actor.init()
-const crawler = new PlaywrightCrawler({
-    headless: true,
-    requestHandlerTimeoutSecs: 99_999,
-    minConcurrency: 1,
-    maxConcurrency: 1,
-    maxRequestRetries: 3,
-    useSessionPool: true,
-    persistCookiesPerSession: true,
-    sessionPoolOptions: { persistStateKey: 'pinterest-data-crawlee', maxPoolSize: 10 },
-    // proxyConfiguration: new ProxyConfiguration({ proxyUrls: ['...'] }),
-    requestHandler: router,
-    preNavigationHooks: preNavigationHooks,
+// const crawler = new PlaywrightCrawler({
+//     headless: true,
+//     requestHandlerTimeoutSecs: 99_999,
+//     minConcurrency: 1,
+//     maxConcurrency: 1,
+//     maxRequestRetries: 3,
+//     useSessionPool: true,
+//     persistCookiesPerSession: true,
+//     sessionPoolOptions: { persistStateKey: 'pinterest-data-crawlee', maxPoolSize: 10 },
+//     // proxyConfiguration: new ProxyConfiguration({ proxyUrls: ['...'] }),
+//     requestHandler: router,
+//     // preNavigationHooks: preNavigationHooks,
 
-});
-// Slow mode
-// crawler.launchContext.launchOptions = { slowMo: 500 }
+// });
+// // Slow mode
+// // crawler.launchContext.launchOptions = { slowMo: 500 }
 
-await crawler.addRequests(startUrls)
+// await crawler.addRequests(startUrls)
 
-await crawler.run().then(async () => {
-    log.info('Done, will now exit...');
-    await crawler.teardown()
-});
+// await crawler.run().then(async () => {
+//     log.info('Done, will now exit...');
+//     await crawler.teardown()
+// });
+// NODE VERSION
+let userName = 'USRENAME'
+let pins_url_bookmark = (userName: string, bookmark: string) => `https://www.pinterest.ca/resource/UserPinsResource/get/?source_url=%2F${userName}%2Fpins%2F&data=%7B%22options%22%3A%7B%22is_own_profile_pins%22%3Atrue%2C%22username%22%3A%22${userName}%22%2C%22field_set_key%22%3A%22grid_item%22%2C%22pin_filter%22%3Anull%2C%22bookmarks%22%3A%5B%22${bookmark}%22%5D%7D%2C%22context%22%3A%7B%7D%7D&_=1670393784068`
+
+let query = pins_url_bookmark(userName, '')
+let list: any[] = []
+let i = 0
+let timer = setInterval(async () => {
+    console.log(query)
+    let response_json = await (await fetch(query)).json();
+    let bookmark = response_json.resource.options.bookmarks[0];
+    query = pins_url_bookmark(userName, bookmark)
+
+    let pins = response_json.resource_response.data
+    list.push(pins)
+
+    console.log(`list length: ${list.length}`);
+    log.info(`list length: ${list.length}`);
+
+    if (bookmark.includes('end')) {
+        console.log('end');
+
+        ds.pushData((list.flat()))
+
+        console.log("saved to file");
+        clearInterval(timer)
+        return
+    }
+    i++
+}, 2000)
+
+log.info('Done, will now exit...');
 
 await Actor.exit()
