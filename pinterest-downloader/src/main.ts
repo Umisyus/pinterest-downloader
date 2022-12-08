4b062042875b9d2484bc27b8478992f4485aabe0
umit <umit007_@hotmail.com>
2022-12-08 05:07:13 -0500

    Downloads images

--- pinterest-downloader/src/main.ts
+++ pinterest-downloader/src/main.ts
@@ -1,12 +1,35 @@
 // For more information, see https://crawlee.dev/
-import { PlaywrightCrawler, ProxyConfiguration } from 'crawlee';
+import { ApifyClient } from 'apify';
+import { PlaywrightCrawler } from 'crawlee';
 import { router } from './routes.js';
+const client = new ApifyClient();
+client.token = process.env.APIFY_TOKEN;
+client.baseUrl = 'https://api.apify.com/v2/';
 
-const startUrls = ['https://crawlee.dev'];
+var dataSet = await client.dataset('0UiUYmR0kikTmpgLX' ?? 'pinterest-json').listItems({
+    // fields: [
+    //     'images/orig/url',
+    //     'grid_title',
+    //     'id',
+    //     'board/name',
+    //     'board/url'
+    // ]
+})
+
+console.log(dataSet);
+export const ds = dataSet;
+
+const startUrls = dataSet.items.map((item: any) => item.images.orig.url);
 
 const crawler = new PlaywrightCrawler({
     // proxyConfiguration: new ProxyConfiguration({ proxyUrls: ['...'] }),
     requestHandler: router,
+    maxConcurrency: 5,
+    minConcurrency: 1,
+    maxRequestRetries: 3,
+    // TEST
+    maxRequestsPerCrawl: 100,
+    maxRequestsPerMinute: 10
 });
 
 await crawler.run(startUrls);
--- pinterest-downloader/src/routes.ts
+++ pinterest-downloader/src/routes.ts
@@ -1,21 +1,58 @@
 import { Dataset, createPlaywrightRouter } from 'crawlee';
 
+import fs from 'fs';
+import { ds } from './main.js';
 export const router = createPlaywrightRouter();
+const baseFolder = './images/';
+// let report = (await Dataset.open('completed-downloads'))
+let folderPath = '';
+router.addDefaultHandler(async ({ log, request, response }) => {
 
-router.addDefaultHandler(async ({ enqueueLinks, log }) => {
-    log.info(`enqueueing new URLs`);
-    await enqueueLinks({
-        globs: ['https://crawlee.dev/**'],
-        label: 'detail',
-    });
-});
+    log.info('Downloading the page... ' + request.url);
+    if (response && response?.ok()) {
+        let body = await response.body();
+        let text = await response.text();
 
-router.addHandler('detail', async ({ request, page, log }) => {
-    const title = await page.title();
-    log.info(`${title}`, { url: request.loadedUrl });
+        let filename = ''
+        // EX. URL: *pinterest-pin-id[.png,.jpg,.webp]
+        let url_title = request.url.split('/')[request.url.split('/').length - 1];
+        let item = ds.items.find((item: any) => item.images.orig.url === request.url);
+        let innerFolder = '';
+        if (item) {
+            const { grid_title, board } = item as any;
+            let boardName = board.name;
+            innerFolder = boardName + '/';
+            if (grid_title) {
+                filename = grid_title.replace(/[^a-zA-Z0-9]|\\+\//g, '-').replace(/-{2,}/g, '-').replace(/^-|-$/, '')
+            } else {
 
-    await Dataset.pushData({
-        url: request.loadedUrl,
-        title,
-    });
+                filename = url_title;
+            }
+        }
+        // TODO: Prevent recursive folder creation
+        // TODO: Prevent duplicate file names
+        folderPath = baseFolder + innerFolder;
+        // Note: This is a checking if the FOLDER EXISTS, if not, create it
+        if (!fs.existsSync(folderPath)) {
+            fs.mkdir(folderPath, () => log.info("Created folder: " + folderPath));
+        }
+
+        // Save the file with board name and pin title or url id
+        let file = fs.createWriteStream(folderPath + filename)
+
+        file.write(body ?? text);
+
+        file.on('finish', async () => {
+            log.info('Downloaded the page... ' + request.url);
+            // report.pushData({ url: request.url, completed: true })
+
+            file.close();
+        })
+        file.on('error', (err: Error) => {
+            log.error(err.message)
+            // report.pushData({ url: request.url, completed: false })
+
+        });
+    }
+
 });
