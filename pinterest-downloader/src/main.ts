2dcfa7721116ef0b66f57d048aeddcc5c42e82ae
umit <umit007_@hotmail.com>
2022-12-08 18:42:39 -0500

    Input validation

--- pinterest-downloader/src/main.ts
+++ pinterest-downloader/src/main.ts
@@ -2,15 +2,28 @@
 import { Actor, ApifyClient } from 'apify';
 import { PlaywrightCrawler } from 'crawlee';
 import { router } from './routes.js';
-const client = new ApifyClient({ token: process.env.APIFY_TOKEN || '' });
-client.baseUrl = 'https://api.apify.com/v2/';
 await Actor.init()
+const { APIFY_TOKEN, APIFY_USERNAME, DATASET_NAME } = await Actor.getInput<any>();
 
-console.debug(process.env.APIFY_TOKEN?.slice(0, 10))
-console.debug(client.token?.slice(0, 10))
+const client = new ApifyClient({ token: APIFY_TOKEN || process.env.APIFY_TOKEN });
+client.baseUrl = 'https://api.apify.com/v2/';
+if (!APIFY_TOKEN && !process.env.APIFY_TOKEN) {
+    console.log('No APIFY_TOKEN provided!');
+    await Actor.exit({ exit: true, exitCode: 1, statusMessage: 'No APIFY_TOKEN provided!' });
+}
 
-const dataSetName = 'qBxQYcv2AN2xR5imr/pinterest-json';
+if (!APIFY_USERNAME && !process.env.APIFY_USERNAME) {
+    console.log('No APIFY_USERNAME provided!');
+    await Actor.exit({ exit: true, exitCode: 1, statusMessage: 'No APIFY_USERNAME provided!' });
+}
+if (!DATASET_NAME && !process.env.DATASET_NAME) {
+    console.log('No DATASET_NAME provided!');
+    await Actor.exit({ exit: true, exitCode: 1, statusMessage: 'No DATASET_NAME provided!' });
+}
+
+const dataSetName = `${APIFY_USERNAME}/${DATASET_NAME}`;
 // '0UiUYmR0kikTmpgLX' ??
+
 var dataSet = await client.dataset(dataSetName)
     .listItems({
         // fields: [
@@ -28,7 +41,6 @@ export const ds = dataSet;
 const startUrls = dataSet.items.map((item: any) => item.images.orig.url);
 console.log({ startUrls });
 
-process.exit(0);
 const crawler = new PlaywrightCrawler({
     // proxyConfiguration: new ProxyConfiguration({ proxyUrls: ['...'] }),
     requestHandler: router,
