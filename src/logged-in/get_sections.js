69e06363a15ab0c3cfe011adae47efc1c311dd0d
umit <umit007_@hotmail.com>
2022-12-08 19:36:39 -0500

    remove

--- src/logged-in/get_sections.js
+++
@@ -1,4 +0,0 @@
-function get_sections() {
-    let sections = $x('*//div[starts-with(@data-test-id,"section")]')
-    return sections.map(i => (window.location.origin + i.querySelector('a').getAttribute('href')))
-}
--- src/main.ts
+++
@@ -1,5 +0,0 @@
-import { launch_login, crawl_start } from "./crawler.js";
-
-await launch_login()
-    .then(async (page) => await crawl_start(page))
-    .then(() => process.exit(0))
