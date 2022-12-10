5131c57c461af6804ea964cc69f7daf6abd52530
umit <umit007_@hotmail.com>
2022-12-10 00:07:51 -0500

    Extension reading for better store compatibility

--- src/routes.ts
+++ src/routes.ts
@@ -1,3 +1,4 @@
+import { KeyValueStore } from 'apify';
 import { createPlaywrightRouter, Dataset } from 'crawlee';
 
 import fs from 'fs';
@@ -14,6 +15,7 @@ router.addDefaultHandler(async ({ log, request, response }) => {
         let text = await response.text();
 
         let filename = ''
+        let ext = 'png'
         // EX. URL: *pinterest-pin-id[.png,.jpg,.webp]
         let item = imageset.find((item: any) => item.images.orig.url === request.url);
 
@@ -21,29 +23,36 @@ router.addDefaultHandler(async ({ log, request, response }) => {
         if (item) {
             const { grid_title, board } = item as any;
             let boardName = board.name;
+            let boardUrl = board.url;
             innerFolder = boardName + '/';
             if (grid_title) {
                 filename = grid_title
                     .trim().slice(0, 69)
                     .replace(/[^a-zA-Z0-9]|\\+\//g, '-').replace(/-{2,}/g, '-').replace(/^-|-$/, '') + '.png'
             } else {
-                let id = item.id + '.png'
-                filename = request.url.split('/')?.pop() ?? id;
+                // Source: https://www.bannerbear.com/blog/how-to-download-images-from-a-website-using-puppeteer/#downloading-images-from-a-website
+                const matches = /.(jpg|png|svg|gif)/.exec(response.url());
+                if (matches && matches.length > 1) {
+                    ext = matches[1];
+                }
+
+                let pin_url_id = request.url.split('/').filter(Boolean).pop()
+                let id_ext = item.id + `.${ext}`
+                filename = pin_url_id ?? id_ext;
             }
 
             // TODO: Prevent recursive folder creation
             // TODO: Prevent duplicate file names √
-            folderPath = baseFolder + innerFolder;
-            // Note: This is a checking if the FOLDER EXISTS, if not, create it
-            if (!fs.existsSync(folderPath)) {
-                fs.mkdir(folderPath, () => log.info("Created folder: " + folderPath));
-            }
 
             /* Save image */
             // Save with file name or ID?
 
-            await imageKeyValueStore.setValue(filename, body, { contentType: 'image/png' });
-            log.info("Saved image: " + filename);
+            let boardURLName = boardUrl[boardUrl.length - 1] === '/' ? boardUrl.split('/').filter(Boolean).pop() : boardUrl.split('/').pop();
+            // Save image to store in it's board folder
+            let boardImageStore = await KeyValueStore.open(boardURLName as string);
+            // Save image to store as it's specific type (png, jpg, gif, etc.)
+            await boardImageStore.setValue(`${filename}`, body, { contentType: `image/${ext}` });
+            log.info("Saved image: " + filename + " to " + `${boardName}/${filename}`);
         }
 
     }
