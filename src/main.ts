f0ea8728f5663e69a6d938dc390a406a9083378b
umit <umit007_@hotmail.com>
2022-12-11 23:06:32 -0500

    WIP-Get key value stores

--- src/main.ts
+++ src/main.ts
@@ -1,110 +1,70 @@
 // For more information, see https://crawlee.dev/
-import { Actor, ApifyClient, Configuration, Dataset, DatasetContent, KeyValueStore, log, RequestQueue } from 'apify';
-import { Dictionary, PlaywrightCrawler } from 'crawlee';
-import { router } from './routes.js';
+import { Actor, ApifyClient, KeyValueStore, log } from 'apify';
 // import * as tokenJson from "../storage/token.json"
-await Actor.init()
+await Actor.init();
 
-const { APIFY_TOKEN, APIFY_USERNAME, DATASET_NAME, DOWNLOAD_LIMIT = 100 }: { APIFY_TOKEN: string, APIFY_USERNAME: string | undefined | null, DATASET_NAME: string, DOWNLOAD_LIMIT: number | undefined }
-    = await Actor.getInput<any>();
-let token =
-    // tokenJson.token ??
-    APIFY_TOKEN ?? process.env.APIFY_TOKEN
-
+const { APIFY_TOKEN } = await Actor.getInput<any>();
+const token = APIFY_TOKEN ?? process.env.APIFY_TOKEN ?? '';
 
 if (!APIFY_TOKEN && !process.env.APIFY_TOKEN) {
     console.log('No APIFY_TOKEN provided!');
     await Actor.exit({ exit: true, exitCode: 1, statusMessage: 'No APIFY_TOKEN provided!' });
 }
 
-if (!DATASET_NAME && !process.env.DATASET_NAME) {
-    console.log('No DATASET_NAME provided!');
-    await Actor.exit({ exit: true, exitCode: 1, statusMessage: 'No DATASET_NAME provided!' });
-}
-
 export let imageDownloadStatusKeyValueStore = await KeyValueStore.open('completed-downloads');
-
-let vals: any[] = []
-
-// await imageDownloadStatusKeyValueStore
-//     .forEachKey(async (key) => {
-//         let value: any = await imageDownloadStatusKeyValueStore.getValue(key)
-//         if (value?.isDownloaded)
-//             vals.push(value.url)
-//     })
 
 const client = new ApifyClient({ token });
 
 client.baseUrl = 'https://api.apify.com/v2/';
 client.token = token;
 
-// const dataSetName = APIFY_USERNAME ? `${APIFY_USERNAME}/${DATASET_NAME}` : DATASET_NAME;
-
-// export const imageset = ((await Actor.openDataset(dataSetName, { forceCloud: true })).getData({ limit: DOWNLOAD_LIMIT })).catch(console.error)
-//     .then((data) => data?.items ?? []) ?? []
-
-// let startUrls: string[] = []
-// try {
-//     // Extract all the image urls from the dataset
-//     startUrls = ((await imageset))?.map((item: any) => item?.images?.orig?.url) ?? [];
-
-//     log.info(`Total links: ${startUrls.length}`);
-// } catch (e: any) {
-//     console.error(`Failed to read links: ${e}`)
-// }
-
-// // Filter out any pins alreadym marked as downloaded
-// let delta = startUrls.filter((url) => !vals.includes(url))
-// log.info(`Total links downloaded: ${vals.length}`);
-// log.info(`Total links to download: ${delta.length}`);
-// startUrls = delta
-
-// const crawler = new PlaywrightCrawler({
-//     // proxyConfiguration: new ProxyConfiguration({ proxyUrls: ['...'] }),
-//     requestHandler: router,
-//     maxConcurrency: 10,
-//     minConcurrency: 2,
-//     maxRequestRetries: 3,
-//     maxRequestsPerMinute: 100,
-// });
-
-// crawler.addRequests(startUrls.map((url) => ({ url })));
 await downloadZip(client)
-// await crawler.run(startUrls).then(()=>downloadZip(client));
 
 await Actor.exit()
 
-async function downloadZip(client) {
+async function downloadZip(client: ApifyClient) {
 
-// Initialize the ApifyClient with API token
-// const client = new ApifyClient({
-//     token: '<YOUR_API_TOKEN>',
-// });
+    // const input =
+    // {
+    //     "keyValueStoreId": "7TxqCqthXuF9Qmykq",
+    //     "filesPerZipFile": 1000
+    // }
 
-// Prepare actor input
+    const accountKVS = await client.keyValueStores().list({ limit: 5 });
 
-const input =
-{
-  "keyValueStoreId": "7TxqCqthXuF9Qmykq",
-  "filesPerZipFile": 1000
-}
-const downloadsKVS = client.openKeyValueStore('')
-await imageDownloadStatusKeyValueStore
-    .forEachKey(async (key) => {
-        let value: any = await imageDownloadStatusKeyValueStore.getValue(key)
-        if (value?.isDownloaded)
-            vals.push(value.url)
-    })
+    if (accountKVS.items.length == 0) {
+        console.log('No downloads found!');
+        log.info('Abort...');
+        return;
+    }
 
-(async () => {
-    // Run the actor and wait for it to finish
-    const run = await client.actor("jaroslavhejlek/zip-key-value-store").call(input);
+    // Open each key value store and run the actor
+    let [...items] = accountKVS.items
+        // .map(async (kvs) => await Actor.openKeyValueStore(kvs.id));
+        .map(async (kvs) => (kvs.id));
 
-    // Fetch and print actor results from the run's dataset (if any)
-    console.log('Results from dataset');
-    const { items } = await client.dataset(run.defaultDatasetId).listItems();
-    items.forEach((item) => {
-        console.dir(item);
-    });
-})();
+    for await (const i of items) {
+        const input = {
+            "keyValueStoreId": i,
+            "filesPerZipFile": 2000
+        }
+
+        const run = await client.actor("jaroslavhejlek/zip-key-value-store").call(input);
+        const { items } = await client.dataset(run.defaultDatasetId).listItems();
+        items.forEach((item) => {
+            console.dir(item);
+        });
+    }
+
+    // (async () => {
+    //     // Run the actor and wait for it to finish
+    //     const run = await client.actor("jaroslavhejlek/zip-key-value-store").call(input);
+
+    //     // Fetch and print actor results from the run's dataset (if any)
+    //     console.log('Results from dataset');
+    //     const { items } = await client.dataset(run.defaultDatasetId).listItems();
+    //     items.forEach((item: any) => {
+    //         console.dir(item);
+    //     });
+    // })();
 }
