94b349afa73f5ba58bf6c0e5fda92a021ade430f
umit <umit007_@hotmail.com>
2022-12-14 12:47:46 -0500

    create zip using object structure

--- src/fflate-example.js
+++ src/fflate-example.js
@@ -6,46 +6,43 @@ let __dirname = path.dirname(process.argv[1]);
 // This is an ArrayBuffer of data
 const localFile = fs.readFileSync(path.resolve(__dirname + '/../images/Abstract Art/0b1c4669d46a2b2cefe13fb507709efe.jpg'))
 
-// // To use fflate, you need a Uint8Array
-// // Note that Node.js Buffers work just fine as well:
-// let zipFile = new fflate.Zip((err, data, f) => {
-//     if (err) {
-//         console.log(err);
-//     } else {
-//         // fs.writeFileSync('test.zip', data);
-//         console.log(`Zip got something!`);
-//         if (f) {
-//             console.log('File is done!');
-//             fs.writeFileSync('test.zip', data);
-//         }
-//     }
-// })
-// // Higher level means lower performance but better compression
-// // The level ranges from 0 (no compression) to 9 (max compression)
-// // The default level is 6
-// const pngName = 'test.png';
-// let notSoMassive = new fflate.ZipPassThrough(pngName);
+let structure = {}
+let files = [{ k: "name.png", v: localFile }, { k: "name2.png", v: localFile, k: "name3.png", v: localFile, k: "name4.png", v: localFile }]
 
-// notSoMassive.ondata = (err, data) => { if (err) console.error(err); console.log(data); }
-// notSoMassive.filename = pngName;
-// zipFile.add(notSoMassive)
+let i = 0
+let folderName = 'folder'
 
-// notSoMassive.push(localFile, true);
+let arr = []
 
-// zipFile.end();
+const toZip = {
+    'dir1': {
+        'nested': {
+            // You can use Unicode in filenames
+            '你好.txt': fflate.strToU8('Hey there!')
+        },
+        // You can also manually write out a directory path
+        'other/tmp.txt': new Uint8Array([97, 98, 99, 100])
+    },
 
-let b = new Blob([
-    fflate.zipSync({
-        // Directories can be nested structures, as in an actual filesystem
-        dir1: {
-            "test.png": localFile
-        }
-    }, { level: 0 })
-])
+    // PNG is pre-compressed; no need to waste time
+    'superTinyFile.png': [localFile, { level: 0 }],
+};
+const zipped = fflate.zipSync(
+    toZip[localFile] = {localFile}
+);
+for (let f of files) {
+    zipped
+    i++
+}
+let b = new Blob([zipped], { type: 'application/zip' })
 
-b.arrayBuffer().then((data) => {
-    let d = new Uint8Array(data);
-    console.log(d);
-
-    fs.writeFileSync('test.zip', d, { encoding: 'binary' });
+await b.arrayBuffer().then(b => {
+    fs.writeFileSync('test2.zip', zipped)
 })
+
+
+
+//
+// Note that the asynchronous version (see below) runs in parallel and
+// is *much* (up to 3x) faster for larger archives.
+
