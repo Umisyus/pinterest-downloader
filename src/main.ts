ba0e66dbb540e98822a824778a31eb3f38c1c256
umit <umit007_@hotmail.com>
2022-12-25 14:47:45 -0500

    TEST

--- src/main.ts
+++ src/main.ts
@@ -51,19 +51,25 @@ async function zipToKVS(client: ApifyClient) {
 
     async function writeManyZips() {
         // List all key-value stores
-        let kvs_items = await client.keyValueStores().list();
+        let kvs_items = (await client.keyValueStores().list({ offset: 1, limit: 2 })).items
+            .filter((item) => !excluded.includes(item.name ?? item.title ?? item.id));
         // Get the ID and list all keys of the key-value store
-        for (const kvs of kvs_items.items) {
-            let kvs_item = await (Actor.openKeyValueStore(kvs.name ?? kvs.title ?? kvs.id));
+        for (const kvs of kvs_items) {
+            let kvs_item = await Actor.openKeyValueStore(kvs.id, { forceCloud: true });
             // Get the value of each key and save it to the zip file
             // await kvs_item.forEachKey(async (key) => {
             //     let record = await kvs_item.getValue(key);
             //     console.log({ record });
             // })
 
-            let ff = await archiveKVS(kvs_item);
-            let nom = `${randomUUID()}_${kvs.name ?? kvs.title ?? kvs.id}`
-            saveToKVS(ff, nom);
+            let nom = `${randomUUID()}-${kvs.name ?? kvs.title ?? kvs.id}`
+            log.info(`Zipping ${nom} key-value store...`);
+            let ff = await archiveKVS(kvs_item, 10);
+            log.info(`Saving ${nom} to file system...`);
+            saveToFS(ff, undefined, `${nom}.zip`);
+
+            log.info(`Saving ${nom} to key-value store...`);
+            await saveToKVS(ff, nom);
 
             console.log(ff.length);
 
@@ -181,13 +187,13 @@ function getExtension(filename: string) {
     return filename.split('.').pop() ?? '';
 }
 
-async function archiveKVS(store: KeyValueStore) {
+async function archiveKVS(store: KeyValueStore, limit = 100) {
     const buffers: Uint8Array[] = [];
 
     await new Promise<void>(async (resolve, reject) => {
 
         const archive = create('zip', {
-            zlib: { level: 9 } // Sets the compression level.
+            zlib: { level: 0 } // Sets the compression level.
         });
 
         archive.on('data', (chunk: Uint8Array) => {
@@ -201,9 +207,26 @@ async function archiveKVS(store: KeyValueStore) {
 
         archive.on('error', reject);
         // Append each file from the key-value store to the archive
-        await store.forEachKey(async (key) => {
-            const val = await store.getValue(key) as any
-            archive.append(val, { name: `${key}` })
+        let items: any[] = [];
+
+        log.info(`Getting all keys from ${store.name ?? store.id} key-value store...`);
+        // Get all keys from the key-value store
+        let keys: string[] = []
+        await store.forEachKey(async (key) => (<any> keys.push(key)))
+        keys = keys.slice(0, limit)
+        log.info(`Got ${keys.length} keys from ${store.name ?? store.id} key-value store...`);
+
+        let keylength = keys.length;
+        store.forEachKey(async (key, index) => {
+            if (index >= limit) return;
+
+            let value = await store.getValue(key) as any
+            log.info(`Adding ${key} to archive...`);
+            if (!value) console.log(`#${index} was skipped because it was undefined!`, value);
+
+            archive.append(value, { name: `${key}` })
+            log.info(`Added #${index + 1}: ${key} to archive...`);
+            console.log(`#${index} of ${keylength} added to ...`);
         })
 
         archive.finalize()
