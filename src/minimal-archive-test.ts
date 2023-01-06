7d6a7185e85f93280502bc88566b1f46c5b9a3ad
umit <umit007_@hotmail.com>
2023-01-06 12:45:28 -0500

    Test works!

---
+++ src/minimal-archive-test.ts
@@ -0,0 +1,13 @@
+import { Actor, log } from "apify";
+await Actor.init();
+
+log.info("Creating archive...");
+let numbers = Array.from(([...Array(104_620_000).keys()]).map((i) => i * 3_000_000))
+log.info("Archive created");
+let zip = Buffer.from([...numbers]).toString("base64");
+log.info("Archive encoded");
+console.log(zip.length);
+log.info("Saving Archive to KVS...");
+await Actor.setValue("numbers.test", zip, { contentType: "application/zip" });
+log.info("Archive saved");
+await Actor.exit();
