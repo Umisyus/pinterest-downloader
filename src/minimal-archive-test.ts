50e00560af31917e9bf60f9c9d3751f0d9a26729
umit <umit007_@hotmail.com>
2023-01-06 12:49:29 -0500

    TESTING: Same result, less time!

--- src/minimal-archive-test.ts
+++ src/minimal-archive-test.ts
@@ -2,7 +2,7 @@ import { Actor, log } from "apify";
 await Actor.init();
 
 log.info("Creating archive...");
-let numbers = Array.from(([...Array(104_620_000).keys()]).map((i) => i * 3_000_000))
+let numbers = Array(104_620_000).map((i) => i * 3_000_000)
 log.info("Archive created");
 let zip = Buffer.from([...numbers]).toString("base64");
 log.info("Archive encoded");
