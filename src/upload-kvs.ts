d5650ad5d4b580e1071dd53979300d76a419881a
umit <umit007_@hotmail.com>
2023-01-13 14:37:48 -0500

    upload to kvs works

--- src/upload-kvs.ts
+++ src/upload-kvs.ts
@@ -6,16 +6,18 @@ await Actor.init()
 await saveLocalKVSToRemoteKVS("data-kvs", "data-kvs");
 
 async function saveLocalKVSToRemoteKVS(local_kvs_id: string, remote_kvs_id: string) {
-    let local_kvs = await Actor.openKeyValueStore('data-kvs');
+    let local_kvs = await Actor.openKeyValueStore(local_kvs_id);
     let client = new ApifyClient({ token: process.env.TOKEN });
-    client.keyValueStore('data-kvs');
+    let create_kvs = (await client.keyValueStores().getOrCreate(remote_kvs_id)).id;
+    let remote_kvs = client.keyValueStore(create_kvs)
 
     await Actor.openKeyValueStore('data-kvs', { forceCloud: true })
         .then(async (kvs) => {
             await local_kvs.forEachKey(async (k) => {
                 let v = await local_kvs.getValue(k);
                 await kvs.setValue(k, v);
-                console.log(k);
+                await remote_kvs.setRecord({ key: k, value: v as any })
+                    .then(() => console.log(`Uploaded ${k} to remote KVS`));
             });
         });
 }
