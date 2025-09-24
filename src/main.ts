438c8492d16368dcd2159d36348d2478a6547bf3
umit <umit007_@hotmail.com>
2025-09-24 12:48:11 -0400

    + Write files to 'downloads' folder in storage ! Code clean-up ! Zip stub

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import {Actor, ApifyClient, KeyValueStore, log} from 'apify';
 import {FileDownload} from 'crawlee';
 import {Input, Item} from './types.js';
 import {PinData} from './pin-data.js';
+import * as fs from "node:fs";
+import * as Path from "node:path";
+import * as fflate from 'fflate'
 
 await Actor.init()
 
@@ -17,7 +20,7 @@ const {
     APIFY_TOKEN,
     APIFY_USERNAME,
     DATASET_NAME_OR_ID,
-    DOWNLOAD_LIMIT = 1000,
+    DOWNLOAD_LIMIT = 5,
     DATASET_URL = undefined,
     CONCURRENT_DOWNLOADS = 5
 } = input;
@@ -27,7 +30,7 @@ const client = new ApifyClient({token});
 const dataSetToDownload = APIFY_USERNAME ? `${APIFY_USERNAME}/${DATASET_NAME_OR_ID}` : DATASET_NAME_OR_ID;
 const dataseturl = DATASET_URL
 const kvs = await Actor.openKeyValueStore();
-
+const storagePath = Path.join(process.cwd(), 'storage', 'downloads')
 const completedDownloads = 'completed-downloads';
 
 export const getImageSet = async (dataSetNameOrID: string = dataSetToDownload) => {
@@ -52,6 +55,48 @@ export let pin_items: PinData[] = await getImageSet(dataSetToDownload) ?? [];
 
 function findData(url: string, pin_items: PinData[]) {
     return pin_items.find((item: PinData) => item.url === url) ?? null;
+}
+
+async function saveAsFile(filePath: string, fileName: string, body: string | Buffer) {
+    // Create the folder path
+    let folderPath = Path.join(storagePath, filePath)
+    const file_path = Path.join(folderPath, fileName)
+
+    async function downloadFile() {
+
+        return await new Promise((resolve, reject) => {
+            // get a path for the file
+            // save the file to disk
+            const fileWriter = fs.createWriteStream(file_path).on("finish", () => {
+                resolve({});
+            });
+            fileWriter.on('error', (error) => reject(error))
+            fileWriter.write(body)
+            fileWriter.end();
+        })
+
+    }
+
+    if (!fs.existsSync(folderPath)) {
+        fs.mkdirSync(folderPath, {recursive: true})
+    }
+
+    await downloadFile();
+    // await writeFile(fileName, body, contentType)
+}
+
+function getPathForName(url: string) {
+    let fileName;
+    let data = findData(url, pin_items)
+    if (data) {
+        fileName = `${[data.board, data.section].filter(Boolean).map((s: string) => s.substring(0, 15)).join('/')}`
+        if (data.video)
+            fileName = `${[data.board, data.section].filter(Boolean).join('/')}`
+    } else {
+        fileName = `profile-${Date.now()}`
+    }
+    fileName = fileName.replaceAll(/[^a-zA-Z\d]|\s/g, '-');
+    return fileName;
 }
 
 await Actor.main(async () => {
@@ -112,25 +157,36 @@ await Actor.main(async () => {
             const url = new URL(request.url);
 
             let fileName = getFileName(url.href)
-            if (fileName != null) {
-                if (JSON.stringify(body) !== undefined)
-                    await kvs.setValue(fileName, body, {contentType: contentType.type});
-            }
+            let path = getPathForName(url.href)
+            // if (fileName != null) {
+            //     let bodyJSON = JSON.stringify(body)
+            //     if (bodyJSON !== undefined) {
+            // await kvs.setValue(fileName, body, {contentType: contentType.type});
+            await saveAsFile(path, fileName, body)
+                .then(() => console.log(`Wrote file to path: ${Path.join(path, fileName)}`))
+                .catch(error => console.error({error}))
+            // } else throw new Error(`FAILED TO PARSE BODY! ${url}`)
+            // }
 
             log.info(`Downloaded ${fileName} from ${request.url} with content type: ${contentType.type}. Size: ${body?.length} bytes`);
-            // @ts-ignore
-            await imageDownloadStatusKeyValueStore.setValue(fileName),
+            await imageDownloadStatusKeyValueStore.setValue(<string>fileName,
                 {
                     key: url.pathname.split('/').pop(),
                     url:
                     request.url,
                     isDownloaded: true
-                }
+                })
         }
     });
 
-    // crawler.addRequests(startUrls.map((url) => ({ url })));
-    await crawler.run(startUrls);
+    // TEST
+    // startUrls.filter(o => o.includes('.mp4'))
+
+    await crawler.run(startUrls).then(async () => {
+        // Create the zip file here!?
+        await createZipFile(storagePath).then(() => console.log(' TEST: Created!'))
+    });
+
 })
 
 await Actor.exit()
@@ -173,17 +229,26 @@ function normalizePins(ALL_ITEMS: any[]) {
 }
 
 function getFileName(url: string) {
-    let fileName = url.split('/').pop();
+    let fileName;
     // if (!fileName) {
     let data = findData(url, pin_items)
     if (data) {
-        fileName = `${[data.board, data.section, data.name, data.id].filter(Boolean).join('-')}.png`
+        fileName = `${[data.id].filter(Boolean).join('-')}.png`
         if (data.video)
-            fileName = `${[data.board, data.section, data.name, data.id].filter(Boolean).join('-')}.mp4`
+            fileName = `${[data.id].filter(Boolean).join('-')}.mp4`
     } else {
         fileName = `unknown-${Date.now()}.png`
     }
-    // }
 
-    return fileName.replace(/[^a-zA-Z0-9!-_.']/, '-');
+    // fileName = fileName.replaceAll(/[\s\W]+/g, '-');
+
+    // fileName=fileName.replaceAll(/^[a-zA-Z\d]|\s/g, '-');
+    // Filename becomes too long, only board,section,id
+    return fileName
+
+}
+
+async function createZipFile(folderPath: string | Path.ParsedPath) {
+    // Use 'FFlate' library
+
 }
